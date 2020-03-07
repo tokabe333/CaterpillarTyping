1b8f79e615684583c1f52799a41d862bd34a783b
tokabe333 <tokabe333@gmail.com>
2020-03-08 02:09:54 +0900

    add : PhaserTemplate

---
+++ Sources/TS/PhaserSceneTemplate.ts
@@ -0,0 +1,31 @@
+import * as Phaser from "phaser";
+
+export class __ClassName__ extends Phaser.Scene {
+  // --------------- Private変数(undifined可) ---------------
+
+  // --------------- Private変数(undifined不可) ---------------
+
+  // --------------- Phaser用メソッド ---------------
+  
+  // クラスのメンバ変数の初期化
+  init() {
+
+  } //End_Method
+
+  // アセットのロード
+  preload() {
+
+  } //End_Method
+
+  // ゲームオブジェクトの描写
+  create() {
+   
+  } //End_Method
+
+  // ゲームの各フレーム更新毎に呼びだされる
+  update() {
+
+  } //End_Method
+
+  // --------------- 自作メソッド ---------------
+} //End_Class
