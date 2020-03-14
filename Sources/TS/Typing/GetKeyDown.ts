58a52ae8892155315de0f5a1aa69508b7dd57244
tokabe333 <tokabe333@gmail.com>
2020-03-14 20:28:54 +0900

    update : 対応キーを追加(英字キーボードなので意味なさそう)

--- Sources/TS/Typing/GetKeyDown.ts
+++ Sources/TS/Typing/GetKeyDown.ts
@@ -36,6 +36,26 @@ export class GetKeyDown {
     public KeyCommaFirefox: Phaser.Input.Keyboard.Key;
     public KeyCommaForefoxWindows: Phaser.Input.Keyboard.Key;
     public KeyPeriod: Phaser.Input.Keyboard.Key;
+    public Key1: Phaser.Input.Keyboard.Key;
+    public Key2: Phaser.Input.Keyboard.Key;
+    public Key3: Phaser.Input.Keyboard.Key;
+    public Key4: Phaser.Input.Keyboard.Key;
+    public Key5: Phaser.Input.Keyboard.Key;
+    public Key6: Phaser.Input.Keyboard.Key;
+    public Key7: Phaser.Input.Keyboard.Key;
+    public Key8: Phaser.Input.Keyboard.Key;
+    public Key9: Phaser.Input.Keyboard.Key;
+    public Key0: Phaser.Input.Keyboard.Key;
+    public KeySemicolon: Phaser.Input.Keyboard.Key;
+    public KeySemicolonFirefox: Phaser.Input.Keyboard.Key;
+    public KeyColon: Phaser.Input.Keyboard.Key;
+    public KeyForwardSlash: Phaser.Input.Keyboard.Key;
+    public KeyBackSlash: Phaser.Input.Keyboard.Key;
+    public KeyLeft: Phaser.Input.Keyboard.Key;
+    public KeyRight: Phaser.Input.Keyboard.Key;
+    public KeyUp: Phaser.Input.Keyboard.Key;
+    public KeyDown: Phaser.Input.Keyboard.Key;
+    
 
     // キーボード入力取得用オブジェクトをまとめる配列
     public KeyInputs: Phaser.Input.Keyboard.Key[];
@@ -74,6 +94,26 @@ export class GetKeyDown {
         this.KeyCommaFirefox = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.COMMA_FIREFOX);
         this.KeyCommaForefoxWindows = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.COMMA_FIREFOX_WINDOWS);
         this.KeyPeriod = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
+        this.Key1 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
+        this.Key2 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
+        this.Key3 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
+        this.Key4 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
+        this.Key5 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
+        this.Key6 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SIX);
+        this.Key7 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEVEN);
+        this.Key8 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.EIGHT);
+        this.Key9 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NINE);
+        this.Key0 = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ZERO);
+        this.KeySemicolon = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEMICOLON);
+        this.KeySemicolonFirefox = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SEMICOLON_FIREFOX);
+        this.KeyColon = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.COLON);
+        this.KeyForwardSlash = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FORWARD_SLASH);
+        this.KeyBackSlash = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACK_SLASH);
+        this.KeyLeft = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.KeyRight = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.KeyUp = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.KeyDown = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+
 
         // めんどくさいので一つにまとめる
         this.KeyInputs = new Array();
@@ -108,6 +148,25 @@ export class GetKeyDown {
         this.KeyInputs[28] = this.KeyCommaFirefox;
         this.KeyInputs[29] = this.KeyCommaForefoxWindows;
         this.KeyInputs[30] = this.KeyPeriod;
+        this.KeyInputs[31] = this.Key1;
+        this.KeyInputs[32] = this.Key2;
+        this.KeyInputs[33] = this.Key3;
+        this.KeyInputs[34] = this.Key4;
+        this.KeyInputs[35] = this.Key5;
+        this.KeyInputs[36] = this.Key6;
+        this.KeyInputs[37] = this.Key7;
+        this.KeyInputs[38] = this.Key8;
+        this.KeyInputs[39] = this.Key9;
+        this.KeyInputs[40] = this.Key0;
+        this.KeyInputs[41] = this.KeySemicolon;
+        this.KeyInputs[41] = this.KeySemicolonFirefox;
+        this.KeyInputs[41] = this.KeyColon;
+        this.KeyInputs[41] = this.KeyForwardSlash;
+        this.KeyInputs[41] = this.KeyBackSlash;
+        this.KeyInputs[41] = this.KeyLeft;
+        this.KeyInputs[41] = this.KeyRight;
+        this.KeyInputs[41] = this.KeyUp;
+        this.KeyInputs[41] = this.KeyDown;
     } //End_Method
 
     // 何かしらのキーが入力されたとき，それがどのキーか判別する
